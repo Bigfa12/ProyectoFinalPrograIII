cc72fa876695839fa45cac17c55de63edff25240
bauti <anderschbautista@gmail.com>
2025-05-29 09:37:14 -0300

    Merge remote-tracking branch 'origin/ramaBauti' into ramaBauti
    # Conflicts:
    #	pom.xml
    #	src/main/java/com/gimnasio/demo/Controller/ClienteController.java
    #	src/main/java/com/gimnasio/demo/Controller/TrainerController.java
    #	src/main/java/com/gimnasio/demo/Controller/UsuarioController.java
    #	src/main/java/com/gimnasio/demo/Model/Credencial.java

--- src/main/java/com/gimnasio/demo/DemoApplication.java
+++ src/main/java/com/gimnasio/demo/DemoApplication.java
@@ -3,11 +3,14 @@ package com.gimnasio.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDate;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
 
 
 	}
