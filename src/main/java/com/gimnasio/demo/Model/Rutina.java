7600875048b8146f883a16aad8fc3e196cb3a4a3
frantribu <franmartinmdq@gmail.com>
2025-05-29 09:51:36 -0300

    terminamos las entidades, se agrego rutinas

---
+++ src/main/java/com/gimnasio/demo/Model/Rutina.java
@@ -0,0 +1,25 @@
+package com.gimnasio.demo.Model;
+
+import com.gimnasio.demo.Enums.Dia;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Rutina {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id_rutina;
+
+    @Enumerated(EnumType.STRING)
+    private Dia dia;
+
+    @OneToMany(mappedBy = "id_ejercicio")
+    private List<EjercicioRutina> ejercicios;
+}
