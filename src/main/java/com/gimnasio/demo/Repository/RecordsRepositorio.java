0902e72a9c3c826051b2dc25e7837c217b4d3cea
Franco Triboulard <franmartinmdq@gmail.com>
2025-06-08 20:08:20 -0300

    Agregue los records

--- src/main/java/com/gimnasio/demo/Repository/RecordsRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.gimnasio.demo.Repository;
-
-import com.gimnasio.demo.Model.Records;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RecordsRepositorio extends JpaRepository<Records, Long> {
-}
